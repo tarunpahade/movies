49328272f00c4675d7248a283ed93beb6a802dbb
Tarun <tarunpahade6969@gmail.com>
2023-04-02 20:35:06 +0530

    first commit

--- public/index.js
+++ public/index.js
@@ -12,65 +12,69 @@ fetch(API_URL)
         return apidata.json()
     })
     .then(apidata => {
-        for (let i = 0; i < 20; i++) {
-
-
-            const card = document.createElement("div")
-            const h1 = document.createElement("h2")
-            const h2 = document.createElement("h3")
-            const img = document.createElement("img")
-            const p = document.createElement("p")
-            const text = document.createElement("div")
-
-
-            const vte = apidata.results[i].vote_average
-
-
-            h1.innerHTML = apidata.results[i].original_title
-            h2.innerHTML = apidata.results[i].vote_average
-            p.innerHTML = apidata.results[i].overview
-            img.setAttribute("src", img_URL + apidata.results[i].backdrop_path)
-
-
-
-
-            img.classList.add("card-img")
-            card.classList.add("card")
-            text.classList.add("text")
-            p.classList.add("overview")
-
-            text.append(h1, h2, p)
-            card.append(img, text)
-
-            document.querySelector('.cards').append(card)
-
-            if (vte >= "8") {
-
-
-                h2.style.color = "green"
-
-
-
-            }
-            if (vte >= "5" & vte <= "8") {
-                h2.style.color = "yellow"
-            } else if (vte <= "6") {
-
-                h2.style.color = "red"
-            }
-
-        }
+        console.log(apidata);
+        createCard(apidata)
 
     })
 
+function createCard(apidata) {
+    document.querySelector('.cards').innerHTML = ''
+    for (let i = 0; i < 20; i++) {
 
+
+        const card = document.createElement("div")
+        const h1 = document.createElement("h2")
+        const h2 = document.createElement("h3")
+        const img = document.createElement("img")
+        const p = document.createElement("p")
+        const text = document.createElement("div")
+
+
+        const vte = apidata.results[i].vote_average
+
+
+        h1.innerHTML = apidata.results[i].original_title
+        h2.innerHTML = apidata.results[i].vote_average
+        p.innerHTML = apidata.results[i].overview
+        img.setAttribute("src", img_URL + apidata.results[i].backdrop_path)
+
+
+
+
+        img.classList.add("card-img")
+        card.classList.add("card")
+        text.classList.add("text")
+        p.classList.add("overview")
+
+        text.append(h1, h2, p)
+        card.append(img, text)
+
+        document.querySelector('.cards').append(card)
+
+        if (vte >= "8") {
+
+
+            h2.style.color = "green"
+
+
+
+        }
+        if (vte >= "5" & vte <= "8") {
+            h2.style.color = "yellow"
+        } else if (vte <= "6") {
+
+            h2.style.color = "red"
+        }
+
+    }
+}
 
 function getmovies(url) {
-
+    console.log(url);
     fetch(url).then(res => res.json())
         .then(data => {
             console.log(data.results);
-
+            createCard(data)
         })
 }
 
@@ -82,9 +86,8 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     const searchterm = search.value;
     console.log(searchterm);
-    if (searchterm) {
-        getmovies("https://api.themoviedb.org/3/search/movie?api_key=" + API + "&language=en-US&page=1&include_adult=false")
-    }
+    getmovies(`https://api.themoviedb.org/3/search/movie?${API}&query=${searchterm}&page=1&include_adult=true`)
+
 
 
 })
